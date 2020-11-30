ffd81172727fbba5a81717f51b6c6b050d8f8f48
jinwooPark6 <jinwoo13245@naver.com>
2020-11-30 20:04:04 +0900

    17

---
+++ src/main/java/com/example/demo/impls/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.impls;
+
+import com.example.demo.domains.UserDto;
+import com.example.demo.services.UserService;
+
+import org.springframework.stereotype.Service;
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void join(UserDto user) {
+
+    }
+    
+}
---
+++ src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import com.example.demo.domains.UserDto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface UserService {
+    public void join(UserDto user);
+    
+}
